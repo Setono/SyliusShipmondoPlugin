51fb7175994f60f725142d6f65e72b73b0432433
Joachim Løvgaard <joachim@loevgaard.dk>
2023-12-15 14:00:27 +0100

    Adding test (needs rework)

---
+++ tests/DependencyInjection/SetonoSyliusShipmondoExtensionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Setono\SyliusShipmondoPlugin\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Setono\SyliusShipmondoPlugin\DependencyInjection\SetonoSyliusShipmondoExtension;
+
+/**
+ * See examples of tests and configuration options here: https://github.com/SymfonyTest/SymfonyDependencyInjectionTest
+ */
+final class SetonoSyliusShipmondoExtensionTest extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions(): array
+    {
+        return [
+            new SetonoSyliusShipmondoExtension(),
+        ];
+    }
+
+    protected function getMinimalConfiguration(): array
+    {
+        return [
+            'option' => 'option_value',
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function after_loading_the_correct_parameter_has_been_set(): void
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('setono_sylius_shipmondo.option', 'option_value');
+    }
+}
