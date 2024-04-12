5646506acf1b5e01b8806a1c105aace34dae9795
Joachim Løvgaard <joachim@loevgaard.dk>
2024-04-12 09:34:31 +0200

    Allow mapping of payment methods in Sylius to Shipmondo

--- src/Controller/Admin/ShipmondoController.php
+++ src/Controller/Admin/ShipmondoController.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace Setono\SyliusShipmondoPlugin\Controller\Admin;
 
+use Setono\Shipmondo\Client\Client;
+use Setono\Shipmondo\Client\Endpoint\Endpoint;
+use Setono\Shipmondo\Response\PaymentGateways\PaymentGateway;
 use Setono\SyliusShipmondoPlugin\Message\Command\RegisterWebhooks;
+use Setono\SyliusShipmondoPlugin\Model\PaymentMethodInterface;
 use Setono\SyliusShipmondoPlugin\Registrar\WebhookRegistrarInterface;
 use Setono\SyliusShipmondoPlugin\Repository\RegisteredWebhooksRepositoryInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Webmozart\Assert\Assert;
 
 final class ShipmondoController extends AbstractController
 {
@@ -18,12 +25,52 @@ final class ShipmondoController extends AbstractController
         private readonly RegisteredWebhooksRepositoryInterface $registeredWebhooksRepository,
         private readonly MessageBusInterface $commandBus,
         private readonly WebhookRegistrarInterface $webhookRegistrar,
+        private readonly PaymentMethodRepositoryInterface $paymentMethodRepository,
+        private readonly Client $client,
     ) {
     }
 
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        /** @var list<PaymentMethodInterface> $paymentMethods */
+        $paymentMethods = $this->paymentMethodRepository->findAll();
+
+        /** @var list<PaymentGateway> $shipmondoPaymentMethods */
+        $shipmondoPaymentMethods = iterator_to_array(Endpoint::paginate($this->client->paymentGateways()->get(...)));
+
+        // todo this should be made using Symfony forms
+        if ($request->isMethod('POST') && $request->request->has('payment_methods')) {
+            /**
+             * Example of posted array (the key is Sylius payment method id, they value is Shipmondo payment method id):
+             *
+             * [
+             *   1 => "1"
+             *   2 => ""
+             * ]
+             *
+             * @var array<int, string> $postedPaymentMethods
+             */
+            $postedPaymentMethods = $request->request->all('payment_methods');
+            foreach ($postedPaymentMethods as $paymentMethodId => $shipmondoPaymentMethodId) {
+                if ('' === $shipmondoPaymentMethodId) {
+                    continue;
+                }
+
+                /** @var PaymentMethodInterface|null $paymentMethod */
+                $paymentMethod = $this->paymentMethodRepository->find($paymentMethodId);
+                if (null === $paymentMethod) {
+                    continue;
+                }
+
+                Assert::isInstanceOf($paymentMethod, PaymentMethodInterface::class);
+                $paymentMethod->setShipmondoId((int) $shipmondoPaymentMethodId);
+                $this->paymentMethodRepository->add($paymentMethod);
+            }
+        }
+
         return $this->render('@SetonoSyliusShipmondoPlugin/admin/shipmondo/index.html.twig', [
+            'paymentMethods' => $paymentMethods,
+            'shipmondoPaymentMethods' => $shipmondoPaymentMethods,
             'registeredWebhooks' => $this->registeredWebhooksRepository->findOneByVersion($this->webhookRegistrar->getVersion()),
         ]);
     }
