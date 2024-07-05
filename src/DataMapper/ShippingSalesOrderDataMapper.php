3909fbe11b7a16141aa0e082b673cdaaaa2957cf
Joachim Løvgaard <joachim@loevgaard.dk>
2024-07-05 11:17:49 +0200

    Fix possible division by zero

--- src/DataMapper/ShippingSalesOrderDataMapper.php
+++ src/DataMapper/ShippingSalesOrderDataMapper.php
@@ -33,23 +33,29 @@ final class ShippingSalesOrderDataMapper implements SalesOrderDataMapperInterfac
             $shippingAdjustment = $shippingAdjustments->first();
             Assert::isInstanceOf($shippingAdjustment, AdjustmentInterface::class);
 
-            $taxAdjustments = $shipment->getAdjustments(AdjustmentInterface::TAX_ADJUSTMENT);
-            Assert::lessThanEq($taxAdjustments->count(), 1);
-
             // Because of the way shipping and shipping taxes are done in Sylius, we need to do some gymnastics to calculate the correct unit price.
             // If the tax adjustment is neutral we know that the tax is included in the shipping adjustment's amount and hence we subtract the tax.
             // If the tax adjustment is not neutral we know that the tax is not included and hence we don't subtract the tax.
             // The tax is still the same though, and we need that to calculate the vat percentage
-            $tax = 0;
+            $vatPercentage = 0.0;
             $unitPriceExcludingVat = $shippingAdjustment->getAmount();
 
-            $taxAdjustment = $taxAdjustments->first();
-            if (false !== $taxAdjustment) {
-                $tax = $taxAdjustment->getAmount();
+            if ($unitPriceExcludingVat > 0) {
+                $taxAdjustments = $shipment->getAdjustments(AdjustmentInterface::TAX_ADJUSTMENT);
+                Assert::lessThanEq($taxAdjustments->count(), 1);
 
-                if ($taxAdjustment->isNeutral()) {
-                    $unitPriceExcludingVat -= $tax;
+                $tax = 0;
+
+                $taxAdjustment = $taxAdjustments->first();
+                if (false !== $taxAdjustment) {
+                    $tax = $taxAdjustment->getAmount();
+
+                    if ($taxAdjustment->isNeutral()) {
+                        $unitPriceExcludingVat -= $tax;
+                    }
                 }
+
+                $vatPercentage = $tax / $unitPriceExcludingVat;
             }
 
             $orderLine = new OrderLine(
@@ -57,7 +63,7 @@ final class ShippingSalesOrderDataMapper implements SalesOrderDataMapperInterfac
                 itemName: $shippingAdjustment->getLabel(),
                 quantity: 1,
                 unitPriceExcludingVat: formatAmount($unitPriceExcludingVat),
-                vatPercent: (string) ($tax / $unitPriceExcludingVat),
+                vatPercent: (string) $vatPercentage,
                 currencyCode: $order->getCurrencyCode(),
             );
 
